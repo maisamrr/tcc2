a94a2d1551e4c4198afc935fb232e83203b647e7
Gustavo Pedro <you@example.com>
2024-11-22 08:10:29 -0300

    fix: tela de cadastro

--- TCC2/app/lib/screens/subscribe.dart
+++ TCC2/app/lib/screens/subscribe.dart
@@ -53,70 +53,90 @@ class _SubscribeState extends State<Subscribe> {
   }
 
   Future<void> _submitForm(BuildContext context) async {
-  print("Iniciando o processo de cadastro...");
+    print("Iniciando o processo de cadastro...");
 
-  if (!_form.currentState!.validate()) {
-    print("Formulário inválido.");
-    return;
-  }
+    if (!_form.currentState!.validate()) {
+      print("Formulário inválido.");
+      return;
+    }
 
-  String password = _passwordController.text;
-  String? errorPassword = _validatePassword(password);
+    String password = _passwordController.text;
+    String? errorPassword = _validatePassword(password);
 
-  if (errorPassword != null) {
-    print("Erro na senha: $errorPassword");
-    setState(() {
-      _errorText = errorPassword;
-    });
-    return;
-  }
+    if (errorPassword != null) {
+      print("Erro na senha: $errorPassword");
+      setState(() {
+        _errorText = errorPassword;
+      });
+      return;
+    }
 
-  setState(() => _errorLogin = '');
+    setState(() => _errorLogin = '');
 
-  try {
-    UserStore userStore = Provider.of<UserStore>(context, listen: false);
-    userStore.setName(_nameController.text);
-    userStore.setEmail(_emailController.text);
-    userStore.setPassword(_passwordController.text);
+    try {
+      UserStore userStore = Provider.of<UserStore>(context, listen: false);
+      userStore.setName(_nameController.text);
+      userStore.setEmail(_emailController.text);
+      userStore.setPassword(_passwordController.text);
 
-    UserService userService = UserService();
+      UserService userService = UserService();
 
-    bool emailExists = await userService.isEmailRegistered(userStore.email);
-    print("Email existe: $emailExists");
+      bool emailExists = await userService.isEmailRegistered(userStore.email);
+      print("Email existe: $emailExists");
 
-    if (!emailExists) {
-      await userService.saveUser(
-          name: userStore.name,
-          email: userStore.email,
-          password: userStore.password);
-      print("Usuário cadastrado com sucesso.");
+      if (!emailExists) {
+        await userService.saveUser(
+            name: userStore.name,
+            email: userStore.email,
+            password: userStore.password);
+        print("Usuário cadastrado com sucesso.");
 
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text('Cadastro bem-sucedido'),
+              content: const Text('Seu cadastro foi realizado com sucesso.'),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                    Navigator.pushReplacementNamed(context, '/login');
+                  },
+                  child: const Text('OK'),
+                ),
+              ],
+            );
+          },
+        );
+      } else {
+        print("Email já cadastrado.");
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text('Cadastro mal-sucedido'),
+              content: const Text('Email já cadastrado.'),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text('OK'),
+                ),
+              ],
+            );
+          },
+        );
+      }
+    } catch (e) {
+      print("Erro ao cadastrar usuário: $e");
       showDialog(
         context: context,
         builder: (BuildContext context) {
           return AlertDialog(
-            title: const Text('Cadastro bem-sucedido'),
-            content: const Text('Seu cadastro foi realizado com sucesso.'),
-            actions: [
-              TextButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                  Navigator.pushReplacementNamed(context, '/login');
-                },
-                child: const Text('OK'),
-              ),
-            ],
-          );
-        },
-      );
-    } else {
-      print("Email já cadastrado.");
-      showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: const Text('Cadastro mal-sucedido'),
-            content: const Text('Email já cadastrado.'),
+            title: const Text('Erro'),
+            content: Text('Ocorreu um erro ao tentar cadastrar: $e'),
             actions: [
               TextButton(
                 onPressed: () {
@@ -129,213 +149,198 @@ class _SubscribeState extends State<Subscribe> {
         },
       );
     }
-  } catch (e) {
-    print("Erro ao cadastrar usuário: $e");
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text('Erro'),
-          content: Text('Ocorreu um erro ao tentar cadastrar: $e'),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Text('OK'),
-            ),
-          ],
-        );
-      },
-    );
   }
-}
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundIdColor,
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(32, 40, 32, 80),
-        child: Form(
-          key: _form,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              GestureDetector(
-                child: Icon(
-                  Icons.arrow_back,
-                  size: 32,
-                  color: darkGreyColor,
-                ),
-                onTap: () {
-                  Navigator.of(context).pushReplacementNamed('/login');
-                },
-              ),
-              // espacamento
-              const SizedBox(height: 40),
-              // titulo
-              Text(
-                'Notas\nculinárias',
-                style: TextStyle(
-                    fontSize: 48,
-                    fontWeight: FontWeight.bold,
-                    color: darkGreyColor),
-                textAlign: TextAlign.left,
-              ),
-              // espacamento
-              const SizedBox(height: 16),
-              // titulo
-              Text(
-                'Complete seu cadastro',
-                style: TextStyle(fontSize: 18, color: darkGreyColor),
-                textAlign: TextAlign.left,
-              ),
-              // espacamento
-              const SizedBox(height: 24),
-              // nome
-              TextFormField(
-                controller: _nameController,
-                validator: _validateName,
-                keyboardType: TextInputType.text,
-                decoration: InputDecoration(
-                    labelText: 'Nome',
-                    labelStyle: const TextStyle(
-                      color: Color(0xFFADB5BD),
-                    ),
-                    fillColor: Colors.white,
-                    filled: true,
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(6),
-                      borderSide: const BorderSide(
-                        color: Color(0xFFDEE2E6),
-                        width: 1.0,
-                      ),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(6),
-                      borderSide: const BorderSide(
-                        color: Color(0xFFDEE2E6),
-                        width: 1.0,
-                      ),
-                    )),
-              ),
-              // espacamento
-              const SizedBox(height: 16),
-              // email
-              TextFormField(
-                controller: _emailController,
-                validator: _validateEmail,
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                    labelText: 'E-mail',
-                    labelStyle: const TextStyle(
-                      color: Color(0xFFADB5BD),
-                    ),
-                    fillColor: Colors.white,
-                    filled: true,
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(6),
-                      borderSide: const BorderSide(
-                        color: Color(0xFFDEE2E6),
-                        width: 1.0,
-                      ),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(6),
-                      borderSide: const BorderSide(
-                        color: Color(0xFFDEE2E6),
-                        width: 1.0,
-                      ),
-                    )),
-              ),
-              // espacamento
-              const SizedBox(height: 16),
-              // senha
-              TextFormField(
-                controller: _passwordController,
-                keyboardType: TextInputType.text,
-                obscureText: _obscurePassword,
-                decoration: InputDecoration(
-                  labelText: 'Senha',
-                  labelStyle: const TextStyle(
-                    color: Color(0xFFADB5BD),
-                  ),
-                  fillColor: Colors.white,
-                  filled: true,
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(6),
-                    borderSide: const BorderSide(
-                      color: Color(0xFFDEE2E6),
-                      width: 1.0,
-                    ),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(6),
-                    borderSide: const BorderSide(
-                      color: Color(0xFFDEE2E6),
-                      width: 1.0,
-                    ),
-                  ),
-                  suffixIcon: GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        _obscurePassword = !_obscurePassword;
-                      });
-                    },
+      resizeToAvoidBottomInset:
+          true, // Garante que o teclado não cause overflow
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(32, 40, 32, 80),
+            child: Form(
+              key: _form,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  GestureDetector(
                     child: Icon(
-                      _obscurePassword
-                          ? Icons.visibility
-                          : Icons.visibility_off,
+                      Icons.arrow_back,
+                      size: 32,
+                      color: darkGreyColor,
                     ),
+                    onTap: () {
+                      Navigator.of(context).pushReplacementNamed('/login');
+                    },
                   ),
-                ),
-                onChanged: (text) {
-                  setState(() {});
-                },
-              ),
-              if (_errorText != null)
-                Padding(
-                  padding: const EdgeInsets.only(top: 8, left: 8),
-                  child: Text(
-                    _errorText!,
-                    style: const TextStyle(
-                      color: Colors.red,
-                      fontSize: 12,
-                    ),
-                  ),
-                ),
-              // espacamento
-              const SizedBox(height: 16),
-              // login
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () => _submitForm(context),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color(0xFFFB83E8),
-                    side: const BorderSide(
-                      color: Color(0xFF343A40),
-                      width: 1.0,
-                    ),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(32),
-                    ),
-                  ),
-                  child: const Padding(
-                    padding: EdgeInsets.symmetric(vertical: 16),
-                    child: Text(
-                      'Cadastrar',
-                      style: TextStyle(
-                        color: Color(0xFF343A40),
+                  // espacamento
+                  const SizedBox(height: 40),
+                  // titulo
+                  Text(
+                    'Notas\nculinárias',
+                    style: TextStyle(
+                        fontSize: 48,
                         fontWeight: FontWeight.bold,
+                        color: darkGreyColor),
+                    textAlign: TextAlign.left,
+                  ),
+                  // espacamento
+                  const SizedBox(height: 16),
+                  // titulo
+                  Text(
+                    'Complete seu cadastro',
+                    style: TextStyle(fontSize: 18, color: darkGreyColor),
+                    textAlign: TextAlign.left,
+                  ),
+                  // espacamento
+                  const SizedBox(height: 24),
+                  // nome
+                  TextFormField(
+                    controller: _nameController,
+                    validator: _validateName,
+                    keyboardType: TextInputType.text,
+                    decoration: InputDecoration(
+                        labelText: 'Nome',
+                        labelStyle: const TextStyle(
+                          color: Color(0xFFADB5BD),
+                        ),
+                        fillColor: Colors.white,
+                        filled: true,
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(6),
+                          borderSide: const BorderSide(
+                            color: Color(0xFFDEE2E6),
+                            width: 1.0,
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(6),
+                          borderSide: const BorderSide(
+                            color: Color(0xFFDEE2E6),
+                            width: 1.0,
+                          ),
+                        )),
+                  ),
+                  // espacamento
+                  const SizedBox(height: 16),
+                  // email
+                  TextFormField(
+                    controller: _emailController,
+                    validator: _validateEmail,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                        labelText: 'E-mail',
+                        labelStyle: const TextStyle(
+                          color: Color(0xFFADB5BD),
+                        ),
+                        fillColor: Colors.white,
+                        filled: true,
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(6),
+                          borderSide: const BorderSide(
+                            color: Color(0xFFDEE2E6),
+                            width: 1.0,
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(6),
+                          borderSide: const BorderSide(
+                            color: Color(0xFFDEE2E6),
+                            width: 1.0,
+                          ),
+                        )),
+                  ),
+                  // espacamento
+                  const SizedBox(height: 16),
+                  // senha
+                  TextFormField(
+                    controller: _passwordController,
+                    keyboardType: TextInputType.text,
+                    obscureText: _obscurePassword,
+                    decoration: InputDecoration(
+                      labelText: 'Senha',
+                      labelStyle: const TextStyle(
+                        color: Color(0xFFADB5BD),
+                      ),
+                      fillColor: Colors.white,
+                      filled: true,
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(6),
+                        borderSide: const BorderSide(
+                          color: Color(0xFFDEE2E6),
+                          width: 1.0,
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(6),
+                        borderSide: const BorderSide(
+                          color: Color(0xFFDEE2E6),
+                          width: 1.0,
+                        ),
+                      ),
+                      suffixIcon: GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            _obscurePassword = !_obscurePassword;
+                          });
+                        },
+                        child: Icon(
+                          _obscurePassword
+                              ? Icons.visibility
+                              : Icons.visibility_off,
+                        ),
+                      ),
+                    ),
+                    onChanged: (text) {
+                      setState(() {});
+                    },
+                  ),
+                  if (_errorText != null)
+                    Padding(
+                      padding: const EdgeInsets.only(top: 8, left: 8),
+                      child: Text(
+                        _errorText!,
+                        style: const TextStyle(
+                          color: Colors.red,
+                          fontSize: 12,
+                        ),
+                      ),
+                    ),
+                  // espacamento
+                  const SizedBox(height: 16),
+                  // login
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () => _submitForm(context),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFFFB83E8),
+                        side: const BorderSide(
+                          color: Color(0xFF343A40),
+                          width: 1.0,
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(32),
+                        ),
+                      ),
+                      child: const Padding(
+                        padding: EdgeInsets.symmetric(vertical: 16),
+                        child: Text(
+                          'Cadastrar',
+                          style: TextStyle(
+                            color: Color(0xFF343A40),
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
                       ),
                     ),
                   ),
-                ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
       ),
